9c9eb56cea07f1744f29b5eb380306d409b65278
theeasylearn <theeasylearn@gmail.com>
2024-04-05 11:25:58 +0530

    loop day 1

---
+++ 24_loop1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int count=1;
+    while(count<=108) //== >=
+    {
+        //body of loop
+        printf(" Om Bhur Bhuvassuvaha\n Tatsa viturvarenyam \n Bhargo devasya dhimahi \n Dhiyo yonaha prachidayat \n ----------------------------  %d\n",count);
+        count = count + 1; //109
+    }
+    printf("Om shree gayatri matay namah");
+}
