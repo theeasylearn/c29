b4bdd2027cbc443f3b75d53e10f232d807661325
MahmadHadi <148901878+MahmadHadi@users.noreply.github.com>
2024-04-19 09:10:17 +0530

    20 4 24

--- problems.c
+++ problems.c
@@ -24,32 +24,24 @@
         -> start = 10, end = 5 
         -> prod = 10 * 9 * 8 * 7 * 6 * 5
         -> print(prod)
-    
+
 12 april 24
     using both while and for loop 
         sum of digit 
         product of digit 
     reverse of a number -> 123 -> 321
                         -> 5432 -> 2345
+                        
 13 aprli 24
     write a pro to check if number is polindrome or not
-        polindrome -> 101, 555, 1001
+        polindrome -> 101, 555, 1001 -> num == rnum -> (polindrome number )
     write a pro to check password {int} -> using loop, without loop
         password lenght < 4 -> print("weak pwd")
         length >= 4 -> print("Strong pwd");
 
-15 april 24
-    1 1 1 1
-    1 1 1 1
-    1 1 1 1
-    1 1 1 1
-
-    1 2 3 4
-    1 2 3 4
-    1 2 3 4
-    1 2 3 4
-
-    1 1 1 1
-    2 2 2 2
-    3 3 3 3
-    4 4 4 4
+20 aprli 24
+    all sub patterns
+          *
+        * *
+      * * * 
+    * * * *
