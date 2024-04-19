e25ce40398f95a9f46d7f899e8e8e9330253ab57
MahmadHadi <148901878+MahmadHadi@users.noreply.github.com>
2024-04-19 09:09:30 +0530

    Add files via upload

---
+++ inverted2.c
@@ -0,0 +1,59 @@
+
+// * * * 
+//   * *
+//     * 
+
+#include<stdio.h>
+void main() {
+
+    int i, j;
+
+    for (i = 1; i <= 5; i++) {
+        for (j = 1; j <= i - 1; j++) {
+            printf("# ");
+        }
+        for (j = 5; j >= i; j--) {
+            printf("* ");
+        }
+        printf("\n");
+    } 
+}
+
+// * * * *
+// # * * *
+//     * * 
+//       * 
+
+
+i = 1 
+for (j = 1 ; 1 <= 0; j++) {
+    //  code 
+}
+
+i = 2                 2
+for (j = 1; 2 <= 1 ; j++) {
+    code 
+}
+
+i = 3               2, 3
+for (j = 1; 3 <= 2; j++) {
+    code 
+    code 
+} 
+
+i = 4               2, 3, 4
+for (j = 1; 4 <= 3; j++) {
+    code 
+    code 
+    code 
+}
+
+i = 5               2, 3, 4, 5
+for (j = 1; 5 < 5; j++) {
+    code 
+    code 
+    code 
+    code 
+} 
+
+i = 6
